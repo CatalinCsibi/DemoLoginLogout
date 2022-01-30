506b3c0cf67b7fefaf0c4dd40370c62457bbc3ae
CatalinCsibi <csibicatalin@gmail.com>
2022-01-30 14:56:59 +0200

    Added SecurityConfig class, Modified UserServiceImpl and DemoLoginLogoutApplication.java

--- src/main/java/com/example/DemoLoginLogout/service/UserServiceImpl.java
+++ src/main/java/com/example/DemoLoginLogout/service/UserServiceImpl.java
@@ -1,26 +1,48 @@
 package com.example.DemoLoginLogout.service;
 
-import com.example.DemoLoginLogout.model.Role;
 import com.example.DemoLoginLogout.model.AppUser;
+import com.example.DemoLoginLogout.model.Role;
 import com.example.DemoLoginLogout.repository.RoleRepository;
 import com.example.DemoLoginLogout.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository userRepository;
 
     private final RoleRepository roleRepository;
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = userRepository.findByUsername(username);
+        if(user == null) {
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in database");
+        } else {
+            log.info("User found in the database: {} ", username);
+        }
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getName())));
+
+        return new User(user.getUsername(), user.getPassword(), authorities);
+    }
 
     @Override
     public AppUser saveUser(AppUser user) {
